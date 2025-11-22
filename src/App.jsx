d4cdeab4c202417ff1c7dc6bec28fa1b03a7b0a6
cheesepluschalk <melodyrz.qu@gmail.com>
2025-11-22 17:37:40 -0500

    should show header now testpage name fix in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/testpage" element={<testpage/>}/>
+        <Route path="/testpage" element={<TestPage/>}/>
       </Routes>
     </div>
   )
